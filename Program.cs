27887e574db6519bdfc07c186600ee106e25491a
caiocezar <caiocezar.bezerra@hotmail.com>
2024-10-25 19:10:16 -0300

    Celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("SmartPhone Nokia: ");
+Nokia nokia = new("40028922", "N+", "IMEI DO NOKIA", 300);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Tetris 98");
+
+Console.WriteLine("\nSmartPhone Iphone: ");
+Iphone iphone = new("12345678", "Iphone 19", "IMEI DO IPHONE", 10000);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Crypto Wallet");
+Console.ResetColor();
